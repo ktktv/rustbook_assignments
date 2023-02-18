af7182b388fc5a44d3f4a4e7bbc740532595c1f6
viiibe <butterflyeight@protonmail.com>
2023-02-18 22:18:50 +0100

    Revise the exercises with some new knowledge.

--- ch03/01-temperature/src/main.rs
+++ ch03/01-temperature/src/main.rs
@@ -2,47 +2,62 @@ use std::io;
 
 fn main() {
     loop {
+        let mut buf = String::new();
+
         println!("Please input your temperature");
+        let temperature = loop {
+            buf.clear();
 
-        let mut temperature = String::new();
+            io::stdin()
+                .read_line(&mut buf)
+                .expect("Failed to read line");
 
-        io::stdin()
-        .read_line(&mut temperature)
-        .expect("Failed to read line");
-    
-        let temperature: f64 = match temperature.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+            if let Ok(temperature) = buf.trim().parse() {
+                break temperature;
+            } else {
+                println!("Please input correct temperature");
+                continue;
+            };
         };
 
-        println!("Inputted value is: {}", temperature); 
-
-        let mut is_celsius = String::new();
+        println!("Inputted value is: {}", temperature);
 
         println!("Is the temperature in Celsius? (type true for celsius, false for fahrenheit)");
 
-        io::stdin()
-        .read_line(&mut is_celsius)
-        .expect("Failed to read line");
+        let is_celsius = loop {
+            buf.clear();
 
-        let is_celsius: bool = match is_celsius.trim().parse() {
-            Ok(bul) => bul,
-            Err(_) => continue,
+            io::stdin()
+                .read_line(&mut buf)
+                .expect("Failed to read line");
+
+            if let Ok(is_celsius) = buf.trim().parse() {
+                break is_celsius;
+            } else {
+                println!("Invalid answer!");
+                continue;
+            };
         };
 
         if is_celsius == true {
-            println!("Target temperature is {} degrees Fahrenheit.", convert(temperature, is_celsius));
+            println!(
+                "Target temperature is {:.1} degrees Fahrenheit.",
+                convert(temperature, is_celsius)
+            );
         } else {
-            println!("Target temperature is {} degrees Celsius.", convert(temperature, is_celsius));
+            println!(
+                "Target temperature is {:.1} degrees Celsius.",
+                convert(temperature, is_celsius)
+            );
         }
 
         break;
     }
 }
 
-fn convert (temp: f64, val: bool) -> f64 {
+fn convert(temp: f64, val: bool) -> f64 {
     match val {
-        true => temp * 9.0/5.0 + 32.0,
-        false => temp - 32.0 * 5.0/9.0,
+        true => temp * 9.0 / 5.0 + 32.0,
+        false => temp - 32.0 * 5.0 / 9.0,
     }
 }
--- ch03/02-fibonacci/src/main.rs
+++ ch03/02-fibonacci/src/main.rs
@@ -1,46 +1,48 @@
 use std::io;
 
 fn main() {
-    loop {
+    let fib_n = loop {
+        let mut fib_n = String::new();
         println!("Input the n of fibonacci number");
 
-        let mut fib_n = String::new();
-
-        io::stdin()
-            .read_line(&mut fib_n)
-            .expect("Error reading the string");
-
-        let fib_n: u64 = match fib_n.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+        if let Err(_) = io::stdin().read_line(&mut fib_n) {
+            println!("Error reading the line, try again.");
+            continue;
         };
 
-        fibonacci(fib_n);
+        match fib_n.trim().parse() {
+            Ok(num) => break num,
+            Err(_) => continue,
+        };
+    };
 
-        println!("The {}th Fibonacci number is {}.", fib_n, fibonacci(fib_n));
-        break;
-    }
+    fibonacci(fib_n);
+
+    println!("The {}th Fibonacci number is {}.", fib_n, fibonacci(fib_n));
 }
 
 fn fibonacci(n: u64) -> u64 {
+    if n == 0 {
+        return 0;
+    }
+
+    if n == 1 || n == 2 {
+        return 1;
+    }
+
     let mut f: u64 = 0;
 
-    if n == 0 {
-    } else if n == 1 || n == 2 {
-        f = 1;
-    } else {
-        let mut i = 0;
+    let mut i = 0;
 
-        let mut f1 = 1;
-        let mut f2 = 1;
+    let mut f1 = 1;
+    let mut f2 = 1;
 
-        while i < (n - 2) {
-            f = f1 + f2;
-            f1 = f2;
-            f2 = f;
+    while i < (n - 2) {
+        f = f1 + f2;
+        f1 = f2;
+        f2 = f;
 
-            i += 1;
-        }
+        i += 1;
     }
 
     f
